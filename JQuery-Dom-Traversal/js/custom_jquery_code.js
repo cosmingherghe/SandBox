5e7e0c88f518e489fc4419a42eda85b83339ad54
cosmingherghe <cosminlinux@gmail.com>
2022-12-14 17:11:21 -0600

    updates

--- JQuery-Dom-Traversal/js/custom_jquery_code.js
+++ JQuery-Dom-Traversal/js/custom_jquery_code.js
@@ -9,5 +9,20 @@ $(function(){
       $(this).parent().append(info).css("font-weight","bold");
     })
 
+    //$("span.child").prev().css({"color": "chocolate", "border": "2px solid chocolate"});
+
+    // $("span.child").parents().css({"color": "chocolate", "border": "2px solid chocolate"});
+
+    //$("span.child").parent().css({"color": "chocolate", "border": "2px solid chocolate"});
+
+    //$("span").css({"color": "chocolate", "border": "2px solid chocolate"});
     
+    //$("span").css({"color": "chocolate", "border": "2px solid chocolate"});
+
+    //$("ul").find("li").first().next().css({"color": "chocolate", "border": "2px solid chocolate"});
+    
+    //$("p").parent().css({"color": "chocolate", "border": "2px solid chocolate"});
+
+    //$("li").children().css({"color": "chocolate", "border": "2px solid chocolate"});
+
 });
