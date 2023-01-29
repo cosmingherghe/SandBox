f2e6aecc48a27e344cc4030b882c473d73e8ea7f
cosmingherghe <cosminlinux@gmail.com>
2023-01-29 15:53:32 -0500

    update DTO approach

--- Java/streams2-api/src/main/java/dev/cosmingherghe/App.java
+++ Java/streams2-api/src/main/java/dev/cosmingherghe/App.java
@@ -1,9 +1,12 @@
 package dev.cosmingherghe;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import dev.cosmingherghe.model.Person;
+import dev.cosmingherghe.model.PersonDTO;
 import dev.cosmingherghe.model.UtilData;
 
 public class App {
@@ -11,13 +14,41 @@ public class App {
 
         List<Person> persons = UtilData.getPeople("resources\\people.json", Person.class);
 
-        System.out.println(persons.get(0));
+        // Map Person to PersonDTO
+        // First approach
+        List<PersonDTO> personsListDTO = persons.stream()
+                .map(p -> {
+                    PersonDTO pDto = new PersonDTO(
+                            p.getId(), p.getLastName(), p.getAge());
+                    return pDto;
+                })
+                .collect(Collectors.toList());
 
+        // Second approach
+        List<PersonDTO> personDTOs = persons.stream()
+                .map(p -> UtilData.mapToPersonDTO(p))
+                .collect(Collectors.toList());
+
+        // ------------------------------------------------------
+
+        // Imperative Programming
+        ArrayList<Person> age18OrLess = new ArrayList<>();
+
+        for (int i = 0, k = 0; i < persons.size(); i++) {
+            if (persons.get(i).getAge() <= 18) {
+                System.out.println(persons.get(i));
+                k++;
+                if (k == 10)
+                    break;
+            }
+        }
+
+        // Declarative programming
+        System.out.println("--- Streams ---");
         persons.stream()
-                .filter(p -> p.getId() < 10)
-                .forEach(p -> System.out.println(p.getFirstName()));
-
-        System.out.println("--- Generics ---");
+                .filter(p -> p.getAge() <= 18)
+                .limit(10)
+                .forEach(System.out::println);
 
     }
 }
