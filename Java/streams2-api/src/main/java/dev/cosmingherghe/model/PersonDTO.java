34a2cf0b3be9d6b869de48d638006e3be3284ff8
cosmingherghe <cosminlinux@gmail.com>
2023-01-29 15:53:07 -0500

    add Builder

--- Java/streams2-api/src/main/java/dev/cosmingherghe/model/PersonDTO.java
+++ Java/streams2-api/src/main/java/dev/cosmingherghe/model/PersonDTO.java
@@ -1,12 +1,14 @@
 package dev.cosmingherghe.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class PersonDTO {
 
     private Integer id;
